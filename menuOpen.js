98b2b6a3db84d87cc78522adc4ae03ecb69d1a03
Ranit Jana <ranitjana100@gmail.com>
2024-03-03 18:22:53 +0530

    added

---
+++ menuOpen.js
@@ -0,0 +1,5 @@
+// let btn = document.querySelector('.nav-hide');
+// let navLinks = document.querySelector('.nav__links');
+// btn.addEventListener('click', () => {
+//     navLinks.style = 'left:0px';
+// });
