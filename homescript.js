7984dde63ccd88f6f3f95daf21e57f96d7ef4eac
Ranit Jana <ranitjana100@gmail.com>
2024-03-09 11:10:58 +0530

    add scroll to child

--- homescript.js
+++ homescript.js
@@ -4,6 +4,26 @@ register.addEventListener("click", function () {
 });
 
 let service = document.getElementById('gsv');
-service.addEventListener("click",function(){
+service.addEventListener("click", function () {
     window.location.href = "./Service/service.html";
-})
+});
+
+gsap.from("#HOME,#ABOUT,#INS,#SER,#CONT", {
+    opacity: 0,
+    x: 100,
+    stagger: 0.2,
+    duration: 1
+});
+gsap.from(".content h1", {
+    opacity: 0,
+    x: -50,
+    stagger: 0.1,
+    duration: 0.5
+});
+gsap.from(".content p", {
+    opacity: 0,
+    x: 50,
+    stagger: 0.1,
+    delay:0.2,
+    duration: 1
+});
