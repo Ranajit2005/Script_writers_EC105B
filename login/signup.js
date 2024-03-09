6fa160e1e7ae984741f8742fc0080b76818e8d5c
mdsalmanfarsee <far90986@gmail.com>
2024-03-09 13:04:15 +0530

    authentication

---
+++ login/signup.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBRHNFPt3-RzLpLE3BtmrEaNTgWtnzk1g4",
+    authDomain: "database1-b27c6.firebaseapp.com",
+    projectId: "database1-b27c6",
+    storageBucket: "database1-b27c6.appspot.com",
+    messagingSenderId: "306088001178",
+    appId: "1:306088001178:web:7d7fc1041e2fb23f046923"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
